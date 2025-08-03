67d8b9bb28e17c2e21d7301a70b4aa6d5276878e
Leo dev <klesti.selimay@icloud.com>
2025-08-03 11:25:52 -0500

    single file

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 import keys
-import src_code
-import output_parser
 import os
 import shutil
 import google.generativeai as genai
+import re
 
-prompt = """You are a senior software engineer tasked with documenting a Rust project.
+ignore = []
+
+if os.path.exists('.sparkignore'):
+    with open('.sparkignore', 'r') as f:
+        ignore = f.readlines()
+
+elif os.path.exists('.gitignore'):
+    with open('.gitignore', 'r') as f:
+        ignore = f.readlines()
+
+def file_prompt(path: str, contents: str):
+    return f"File: {path}\nContents: ```\n{contents}\n```\n"
+
+def get_local(path: str) -> str:
+    prompt = ""
+    subs = os.listdir(path)
+
+    for sub in subs:
+        rel = os.path.join(path, sub)
+        if os.path.isdir(rel):
+            get_local(rel)
+        else:
+            with open(rel, 'r') as f:
+                prompt += file_prompt(rel, f.read())
+
+    return prompt
+
+pattern = r"File: (?P<filePath>.*?)\nContents: ```(?:\n|)(?P<content>[\s\S]*?)```\nEND_FILE"
+
+def parse_output(input: str) -> list[tuple[str, str]]:
+    output = []
+
+    for match in re.finditer(pattern, input):
+        file_path = match.group("filePath")
+        content = match.group("content")
+        output.append((file_path, content))
+
+    return output
+
+prompt = """You are a senior software engineer tasked with documenting a project.
 Your output must be structured as follows for each generated documentation file:
 
 File: <path/to/documentation/file.md>
@@ -21,7 +59,7 @@ END_FILE
 Generate the clear and understandable Markdown documentation based on the following source code:
 """
 
-prompt += src_code.get_local('')
+prompt += get_local('')
 
 genai.configure(api_key=keys.API_KEY)
 
@@ -30,7 +68,7 @@ response = model.generate_content(prompt)
 print(response.text)
 print('---------------')
 
-for file in output_parser.parse_output(response.text):
+for file in parse_output(response.text):
     file_path = os.path.join('docs_output', file[0])
     if os.path.exists('docs_output'):
         shutil.rmtree('docs_output')
